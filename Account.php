016e9a1f64d38aaca4135b2945d291f745fc81ce
Karla <alulodkarlamae@gmail.com>
2024-04-12 22:32:51 +0800

    first commit

---
+++ Account.php
@@ -0,0 +1,196 @@
+<?php
+// session_start();
+require 'count-cart.php';
+
+if (!isset($_SESSION['username'])) {
+    header("location:login_form.php");
+    exit();
+}
+
+include('DB/db_con.php');
+
+$username = $_SESSION['username'];
+$role = $_SESSION['role'];
+
+$sql = "SELECT users.first_name, users.last_name, users.phone_number, users.username, users.role, users.email, address.street, address.barangay 
+        FROM users 
+        LEFT JOIN address ON users.address_id = address.address_id 
+        WHERE users.username = :username";
+
+$stmt = $pdo->prepare($sql);
+$stmt->bindParam(':username', $username);
+$stmt->execute();
+
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Account Dashboard</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display+swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/a1e3091ba9.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./scss/style.scss">
+    
+</head>
+<body>
+
+    <div class="navbar">
+        <div class="logo">
+            <img src="images/Logo.png" width="125">
+        </div>
+        <nav id="menuItems">
+        <ul>
+            <?php
+            $dashboardLink = '';
+            switch ($_SESSION['role']) {
+                case 'Admin':
+                    $dashboardLink = 'admin/admin_dash.php';
+                    break;
+                case 'Staff':
+                    $dashboardLink = 'staff_dash.php';
+                    break;
+                case 'Delivery_personnel':
+                    $dashboardLink = 'delivery_dash.php';
+                    break;
+                case 'Customer':
+                    $dashboardLink = 'customer_dash.php';
+                    break;
+                default:
+                    $dashboardLink = 'index.php';
+                    break;
+            }
+            ?>
+            <li><a href="<?php echo $dashboardLink; ?>">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            </ul>
+        </nav>
+        <div class="setting-sec">
+            <!-- <a href="http://localhost/E-commerce/Account.php">
+            <img src="images/profile-icon.png" width="30px" height="30px" class="icon">
+            </a> -->
+            <div class="cart-sec">
+                <a href="http://localhost/E-commerce/cart-view.php">
+                    <span class="cart-count"><?php echo $cart_count; ?></span>
+                    <img src="images/cart.png" width="30px" height="30px">
+                </a>
+            </div>
+            <img src="images/menu.png" class="menu-icon" onclick="menutoggle()">
+        </div>
+    </div>
+    <!-- <header>
+    </header> -->
+<!-- --end-- -->
+<div class= "main-con">
+    <div class="profile-container">
+        <?php
+            $username = isset($_SESSION['username']) ? $_SESSION['username'] : '';
+            if (!empty($username)) {
+                echo '<div class="profile">';
+                    echo '<img src="images/account.png" alt="User Photo">';
+                    echo '<div class="username">' . $username . '</div>';
+                    echo '<div class="logout"><a href="login_form.php">Logout</a></div>';
+                echo '</div>';
+            }
+        ?>
+        <div class="account">
+            <div class="dropdown">
+                <img src="images/account.png" alt="Account Icon">
+                <span>My Account</span>
+                <div class="dropdown-content">
+                    <a href="Account.php"><i class="fa fa-pencil"></i> Edit Profile</a></br>
+                    <!-- <a href="Account.php">Profile</a></br> -->
+                    <a href="address"><i class="fa-solid fa-location-dot"></i>  Addresses</a></br>
+                    <a href="change-pass.php"><i class="fa fa-edit"></i>Change Password</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <img src="images/purchase.png" alt="Purchase Icon">
+                <span>My Purchase</span>
+                <div class="dropdown-content">
+                    <a href="http://localhost/E-commerce/my_orders.php">My Orders</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-account-info">
+        <h2 style="text-align:center">Welcome, <?php echo $_SESSION['username'] ?? 'Guest'; ?></h2>
+        <table class= "tbl-info">
+            <tr class="account-info">
+                <td>First Name:</td>
+                <td><?php echo $user['first_name']; ?></td>
+            </tr>
+            <tr class="account-info">
+                <td>Last Name:</td>
+                <td><?php echo $user['last_name']; ?></td>
+            </tr>
+            <tr class="account-info">
+                <td>Username:</td>
+                <td><?php echo isset($user['username']) ? $user['username'] : ''; ?></td>
+
+            </tr>
+            <tr class="account-info">
+                <td>Email:</td>
+                <td><?php echo $user['email']; ?></td>
+            </tr>
+            <tr class="account-info">
+                <td>Phone Number:</td>
+                <td><?php echo $user['phone_number']; ?></td>
+            </tr>
+            <tr class="account-info">
+                <td>Role:</td>
+                <td><?php echo $user['role']; ?></td>
+            </tr>
+            <tr class="account-info">
+                <td>Street:</td>
+                <td><?php echo $user['street']; ?></td>
+            </tr>
+            <tr class="account-info">
+                <td>Barangay:</td>
+                <td><?php echo $user['barangay']; ?></td>
+            </tr>
+        </table>
+        <div class="button">
+            <a href="http://localhost/E-commerce/Edit-info.php" class="edit-button"><i class="fa fa-edit"></i>Edit</a>
+            <a href="htt p://localhost/E-commerce/change-pass.php" class="change-button"><i class="fa fa-edit"></i>Change Password</a>   
+        </div>
+    </div>
+</div>
+<!--footer-->
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="footer-col-1">
+            <img src="images/logo2.png" width="100px" height="60px">
+            </div>
+            <div class="footer-col-2">
+            <p>&copy; <?php echo date('Y'); ?> 4M Minimart Online Store. All rights reserved.</p>
+            </div>
+        </div>
+    </div>
+    </footer>
+<!-- js for toggle menu -->
+<script>
+    var menuItems = document.getElementById("menuItems");
+    function menutoggle() {
+        menuItems.classList.toggle("show");
+    }
+    // MenuItems.style.maxHeight = "0px";
+    // function menutoggle() {
+    //     if (MenuItems.style.maxHeight =="0px")
+    //     {
+    //         MenuItems.style.maxHeight = "300px";
+    //     }
+    //     else{
+    //         MenuItems.style.maxHeight = "0px";
+    //     }
+    // }
+</script>
+</html>
+</body>
---
+++ Category_items.php
@@ -0,0 +1,67 @@
+<?php
+require 'DB/db_con.php';
+
+if(isset($_POST['categoryId'])) {
+    $category = $_POST['categoryId'];
+
+
+    try {
+        if ($category == 0) {
+            $sql = "SELECT products.*, category.category_name
+                    FROM products
+                    INNER JOIN category ON products.category_id = category.category_id";
+        } else {
+            $sql = "SELECT products.*, category.category_name
+                    FROM products
+                    INNER JOIN category ON products.category_id = category.category_id
+                    WHERE products.category_id = :category";
+        }
+
+        $stmt = $pdo->prepare($sql);
+
+        if ($category != 0) {
+            $stmt->bindParam(':category', $category, PDO::PARAM_INT);
+        }
+
+        $stmt->execute();
+        $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+        foreach ($products as $product) {
+            echo '<div class="product-item">';
+            if (isset($_SESSION['user_id'])) {
+                echo '<a href="product_details.php?product_id=' . $product['product_id'] . '">';
+                echo '<img src="images/upload/' . $product['photo'] . '" alt="Product Photo" class="prod-img">';
+                echo '</a>';
+                // echo '<h3 style="text-align: center;">' . $product['prod_name'] . '</h3>';
+                echo '<h3 style="text-align: center; font-weight: bold;  border-bottom: 1px solid red;">';
+                echo '<a href="product_details.php?product_id=' . $product['product_id'] . '">';
+                echo $product['prod_name'];
+                echo '</a>';
+                echo '</h3>';
+            } else {
+                echo '<a href="login_form.php">';
+                echo '<img src="images/upload/' . $product['photo'] . '" alt="Product Photo" class="prod-img">';
+                echo '</a>';
+                echo '<h3 style="text-align: center;">';
+                echo '<a href="login.php">';
+                echo $product['prod_name'];
+                echo '</a>';
+                echo '</h3>';
+            }
+            echo '<p style="text-align: center;">"Avail 3pcs above to get Discounted price"</p>';
+            echo '<p>Discounted Price: ₱ ' . number_format($product['discounted_price'], 2) . '</p>';
+            echo '<p>Regular Retail Price: ₱ ' . number_format($product['retail_price'], 2) . '</p>';
+            echo '<p>Stock: ' . number_format($product['stock']) . '</p>';
+            // echo '<a href="product_details.php?product_id=' . $product['product_id'] . '" class="details">';
+            // echo '<i class="fa fa-eye"></i> View Details</a>';
+            echo '</div>';
+        }
+    } catch (PDOException $e) {
+        die("PDOException: " . $e->getMessage());
+    } catch (Exception $e) {
+        die("Error: " . $e->getMessage());
+    }
+} else {
+    echo "categoryId parameter not received.";
+}
+?>
